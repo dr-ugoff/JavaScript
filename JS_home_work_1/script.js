a226e6448f18a6c1b333d1a11219f6ad3e2c7c86
Andrey <dr_ugoff@mail.ru>
2023-02-15 10:57:07 +0700

    initial commit

---
+++ JS_home_work_1/script.js
@@ -0,0 +1,12 @@
+// Задание 1
+
+let temperatureC = 15;
+let temperatureF = (9 / 5) * temperatureC+ 32;
+console.log(`t = ${temperatureC}°C (по Цельсию)
+t = ${temperatureF}°F (по Фаренгейту)`);
+
+// Задание 2
+
+let name = 'Андрей';
+let admin = name;
+console.log(admin);
---
+++ seminar_1/script.js
@@ -0,0 +1,57 @@
+// // Задание 1
+// let textFromScript = "Текст из файла script.js";
+// console.log(textFromScript);
+
+// // Задание 2
+// let num = 123;
+// console.log(num);
+
+// let a = 5;
+// a = 7;
+// console.log(a);
+
+// let b = 1 + 2 + 3;
+// console.log(b);
+
+// // Lorem ipsum dolor sit amet consectetur adipisicing elit.
+// /*Lorem ipsum dolor sit amet consectetur adipisicing elit.*/
+
+// // Задание 3
+
+// let c = 10;
+// let d = 2;
+// let sum = c + d;
+// let diff = c - d;
+// let multipl = c * d;
+// let div = c / d;
+// console.log(`c = ${c}
+// d = ${d}
+// sum = ${sum}
+// diff = ${diff}
+// multipl = ${multipl}
+// div = ${div}`);
+// console.log(c + d + sum + diff + multipl + div);
+
+// let a = 1.5;
+// let b = 0.75;
+// console.log(a+b);
+
+let a = -5;
+console.log(-a);
+
+
+// Задание 6
+
+let firstName = "Андрей";
+let lastName = "Другов";
+console.log(firstName, lastName);
+console.log(firstName + " " + lastName);
+console.log(`${firstName} ${lastName}`);
+
+const first = "Java";
+const second = "Script";
+console.log(first + second);
+
+const firstWord = "Hello";
+const secondWord = "World";
+console.log(firstWord + " " + secondWord);
